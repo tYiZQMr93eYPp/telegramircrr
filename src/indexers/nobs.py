35b61bff13d23570a5871d080bc46b0bea10a423
tYiZQMr93eYPp <67542405+tYiZQMr93eYPp@users.noreply.github.com>
2026-07-03 16:21:55 +0200

    fix(indexers): regex pattern matcher groups (#9)
    * fix(trackers): regex pattern matcher groups
    
    * build: bump version
    
    * chore: sync uv.lock
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/indexers/nobs.py
+++ src/indexers/nobs.py
@@ -9,7 +9,7 @@ _FIELDS = {
     "title": r"🎬\s(.+?)\n",
     "category": r"Categor.a:\s(.+?)\n",
     "size": r"Tama.o:\s(.+?)\n",
-    "uploader": r"Subido por:\s(.+?)",
+    "uploader": r"Subido por:\s(.+?)$",
     # NOBS doesn't announce the `Freeleech`, `Double Upload`, `Featured`, and `Refundable` values
 }
 
